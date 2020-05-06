950ef857ca446733ffc232bbc9b122948b7101d1
biack519 <43265287+biack519@users.noreply.github.com>
2020-05-06 18:09:14 +0800

    charge subid

---
+++ add steam games.js
@@ -0,0 +1,8 @@
+jQuery.post(
+                        '//store.steampowered.com/checkout/addfreelicense',
+                        {
+                                action: 'add_to_cart',
+                                sessionid: g_sessionID,
+                                subid:457405
+                        }
+                )
